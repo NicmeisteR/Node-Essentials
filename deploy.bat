ad021c47134a53de3c7268d1399aa56fa7ad1d90
Nicolaas Nel <nicmeister@live.co.za>
2019-12-17 19:06:34 +0200

    v1.1.1

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+echo off
+set arg1=%1
+set arg2=%2
+shift
+shift
+call npm version patch
+call git add -A
+call git commit -m %arg1%
+call git push
+call git tag %arg1%
+call git push origin %arg1%
