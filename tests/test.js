7780bd00c8970e0d995fd3352ed11aa486b4ee66
Nicolaas Nel <nicmeister@live.co.za>
2019-12-17 18:47:15 +0200

    Added instructions.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 let node = require("../src/app");
 
 let data = {
-    "value": "This is a Test Message. Release"
+    "value": "This is a Test Message. use 'git tag', then 'git push origin {tagnum}"
 };
     
 node.writeToFile("tests", "writeToFile", "json", JSON.stringify(data));
