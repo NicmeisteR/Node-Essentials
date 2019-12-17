571df92b9592a54118b042e10bb17f1c101e7707
Nicolaas Nel <nicmeister@live.co.za>
2019-12-17 18:33:14 +0200

    #release

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 let node = require("../src/app");
 
 let data = {
-    "value": "This is a Test Message."
+    "value": "This is a Test Message. Release"
 };
     
 node.writeToFile("tests", "writeToFile", "json", JSON.stringify(data));
