b3f45113a76da76dd4c01829038250d0923c3faa
Nicolaas Nel <nicmeister@live.co.za>
2019-12-17 19:17:47 +0200

    Fixed deploy.

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,12 @@
-
+echo off
+set arg1=%1
+set arg2=%2
+shift
+shift
+call git add -A
 call npm version patch
+call git add -A
+call git commit -m %arg2%
+call git push
+call git tag %arg1%
+call git push origin %arg1%
